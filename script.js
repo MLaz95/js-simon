fce255e6b0f00cbba2a9ae0f557bed49ce593172
marco <lazzari.marco.ml@gmail.com>
2024-02-09 15:05:15 +0100

    create conto alla rovescia in millisecondi

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const timerElement = document.querySelector('#timer');
 
-
-// today's date in milliseconds
-const now = new Date().getTime();
-
 // monday's date in milliseconds
 const targetDate = new Date('Feb 12, 2024 09:30:00').getTime();
 
-// how many milliseconds are left till 9:30 on monday 12th
-const waitTime = targetDate - now;
+// the interval constantly updates the difference between now and monday
+// and then prints the result on screen
+setInterval(function() {
 
-console.log(now)
-console.log(targetDate)
-console.log(waitTime);
+    // the current date in milliseconds
+    const now = new Date().getTime();
 
+    // how many milliseconds are left till 9:30 on monday the 12th
+    const waitTime = targetDate - now;
+
+    // print result on screen
+    timerElement.innerHTML = waitTime;
+}, 1);
